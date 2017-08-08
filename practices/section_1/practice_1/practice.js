832a9c22973164859731b71637af19b6b206b92a
zhangshuanyu <849125536@qq.com>
2017-08-08 16:52:34 +0800

    first

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var collectionSame=[];
+  var k=0;
+  for( var i=0; i<collection_a.length;i++){
+    for( var j=0;j<collection_b.length;j++){
+      if(collection_a[i] === collection_b[j]){
+        collectionSame[k++] = collection_b[j];
+      }
+    }
+  }
+  return collectionSame;
+
 }
 
 module.exports = collect_same_elements;
--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,18 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var collectionSame=[];
+  var k=0;
+  for(var i=0;i<collection_a.length;i++){
+    for(var j=0;j<collection_b.length;j++){
+      for(var n=0;n<collection_b[j].length;n++){
+        if(collection_a[i] === collection_b[j][n]){
+          collectionSame[k++]=collection_b[j][n];
+        }
+      }
+    }
+  }
+  return collectionSame;
+
 }
 
 module.exports = collect_same_elements;
