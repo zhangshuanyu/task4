225fde4b18c5b2ae48ba0acd0af7d382b181ecff
zhangshuanyu <849125536@qq.com>
2017-08-08 17:07:23 +0800

    second commit

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,17 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var collectionSame=[];
+  var k=0;
+  for(var i=0;i<collection_a.length;i++){
+    for(var j=0;j<object_b.value.length;j++){
+      if(collection_a[i] === object_b.value[j]){
+        collectionSame[k++]=object_b.value[j];
+      }
+    }
+  }
+  return collectionSame;
+  //return '实现练习要求，并改写该行代码。';
+
 }
 
 module.exports = collect_same_elements;
