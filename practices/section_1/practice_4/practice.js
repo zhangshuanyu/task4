4e39d093620c29bee7f3e3fb695ac26bbecff09f
zhangshuanyu <849125536@qq.com>
2017-08-08 17:28:24 +0800

    第三次提交

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,16 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var collectionSame=[];
+  var k=0;
+  for(var A in collection_a){
+    for(var B in object_b.value){
+      if(collection_a[A].key === object_b.value[B]){
+        collectionSame[k++]=object_b.value[B];
+      }
+    }
+  }
+  return collectionSame;
+
 }
 
 module.exports = collect_same_elements;
