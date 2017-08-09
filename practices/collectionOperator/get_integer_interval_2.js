e0753dc64b9cd33f76e8e606ac1c1d2b60fca3fc
zhangshuanyu <849125536@qq.com>
2017-08-09 20:31:43 +0800

    080903

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,7 +2,20 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  if (number_a > number_b) return getEvenInterval(number_a, number_b);
+  if (number_a < number_b) return getEvenInterval(number_b, number_a).reverse();
+  if (number_a % 2 === 0) return [number_a];
 
+  return [];
+}
+
+function getEvenInterval(maxNumber, minNumber) {
+  let evenInterval = [];
+  maxNumber = maxNumber % 2 === 0 ? maxNumber : maxNumber - 1;
+  for (; maxNumber >= minNumber; maxNumber -= 2)
+    evenInterval.push(maxNumber);
+
+  return evenInterval;
 
 }
 
--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,11 +1,31 @@
 'use strict';
 var even_group_calculate_average = function(collection){
-  var a=[4,56,556];
-  var b=[0];
-  var c=[218];
-  return a;
-  return b;
-  return c;
+  let result = [0, 0, 0, 0, 0, 0];
+  let isEven = [false, false, false];
+
+  for (let index = 1; index < collection.length; index += 2) {
+    if (collection[index] % 2 === 0) {
+      isEven[2] = true;
+      if (collection[index] < 10) {
+        result[0] += collection[index];
+        result[3] += 1;
+        isEven[1] = true;
+      } else if (collection[index] < 100 && collection[index] >= 10) {
+        result[1] += collection[index];
+        result[4] += 1
+        isEven[2] = true;
+      } else if (collection[index] < 1000 && collection[index] >= 100) {
+        result[5] += 1
+        result[2] += collection[index];
+      }
+    }
+  }
+
+  if (!isEven[2]) return [0];
+
+  if (isEven[0] || isEven[1]) return [result[0] / result[3], result[1] / result[4], result[2] / result[5]];
+
+  return [result[2] / result[5]];
 
 
 };
